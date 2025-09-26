b72bb0bb15e067a0d57d6990e6042bb8e4486e06
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-09-26 11:40:56 -0400

    feat: add encryption to disko configuration
    - consider breaking down into modules?
    - the amount of indeting is a little unreadable

--- disk-config.nix
+++ disk-config.nix
@@ -1,30 +1,74 @@
-# Example to create a bios compatible gpt partition
-{lib, ...}: {
+# this is a configuration for a single disk.
+# the first partiton contains the unencrypted boot partition,
+# the rest of the disk is encrypted and contains the logical volumes.
+#
+{systemSettings, ...}: {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/nvme0n1";
+    # partition disks
+    main = {
       type = "disk";
+      inherit (systemSettings) device;
       content = {
         type = "gpt";
         partitions = {
-          esp = {
-            name = "ESP";
-            size = "512M";
+          # define the boot partiton
+          ESP = {
+            size = "1G";
             type = "EF00";
             content = {
               type = "filesystem";
-              format = "vfat";
+              format = "fat32";
               mountpoint = "/boot";
+              mountOptions = ["umask=0077"];
             };
           };
-          nixos = {
-            name = "nixos";
+          # define luks partition
+          luks = {
             size = "100%";
-            type = "EF00";
+            content = {
+              type = "luks";
+              name = "crypted";
+              settings = {
+                # point disko to your keykeyFile
+                keyFile = "/tmp/secret.key";
+                allowDiscards = true;
+              };
+              content = {
+                type = "lvm_pv";
+                vg = "vg";
+              };
+            };
+          };
+        };
+      };
+    };
+    # define volume group and logical volumes
+    lvm_vg = {
+      vg = {
+        type = "lvm_vg";
+        # define logical volumes
+        lvs = {
+          swap = {
+            size = "4G";
+            content = {
+              type = "swap";
+              discardPolicy = "both";
+            };
+          };
+          root = {
+            size = "32G";
             content = {
               type = "filesystem";
-              format = "btrfs";
+              format = "ext4";
               mountpoint = "/";
+            };
+          };
+          home = {
+            size = "80%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
             };
           };
         };
