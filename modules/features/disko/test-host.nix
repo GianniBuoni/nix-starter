71f03f91fff7c5c2877d4cf6c542efbb6e83d437
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-11-08 00:08:42 -0500

    refactor: use sops to define luks partition key

--- modules/features/disko/test-host.nix
+++ modules/features/disko/test-host.nix
@@ -8,65 +8,63 @@
       format = "ext4";
     };
   };
-  # define the boot partiton
-  ESP = {
-    size = "1G";
-    type = "EF00";
-    content = {
-      type = "filesystem";
-      format = "vfat";
-      mountpoint = "/boot";
-      mountOptions = ["umask=0077"];
-    };
-  };
-  # define luks partition
-  luks = {
-    size = "100%";
-    content = {
-      type = "luks";
-      name = "crypted";
-      # interactive login
-      passwordFile = "/tmp/secret.key";
-      # point disko to your keykeyFile
-      settings.allowDiscards = true;
-      content = {
-        type = "lvm_pv";
-        vg = "vg";
-      };
-    };
-  };
-  # define logical volumes
-  lvs = {
-    swap = {
-      size = "4G";
-      content = {
-        type = "swap";
-        discardPolicy = "both";
-      };
-    };
-    root = mkExt4 "32G" "/";
-    home = mkExt4 "80%" "/home";
-  };
+  key = "hosts/test_host/luks_key";
 in {
   flake.modules.nixos.disko-test-host = {
     imports = [config.flake.modules.nixos.disko];
 
-    disko.devices = {
-      disk = {
-        ssd = {
-          type = "disk";
-          device = "/dev/nvme0n1";
+    sops.secrets.${key} = {};
+
+    disko.devices.disk.ssd = let
+      # define the boot partiton
+      ESP = {
+        size = "1G";
+        type = "EF00";
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+          mountOptions = ["umask=0077"];
+        };
+      };
+      # define luks partition
+      luks = {
+        size = "100%";
+        content = {
+          type = "luks";
+          name = "crypted";
+          passwordFile = config.sops.secrets.${key}.path;
+          settings.allowDiscards = true;
           content = {
-            type = "gpt";
-            partitions = {
-              inherit ESP luks;
-            };
+            type = "lvm_pv";
+            vg = "vg";
           };
         };
       };
-      lvm_vg.vg = {
-        type = "lvm_vg";
-        inherit lvs;
+    in {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          inherit ESP luks;
+        };
+      };
+    };
+
+    # define logical volumes
+    disko.devices.lvm_vg.vg = {
+      type = "lvm_vg";
+      lvs = {
+        swap = {
+          size = "4G";
+          content = {
+            type = "swap";
+            discardPolicy = "both";
+          };
+        };
+        root = mkExt4 "32G" "/";
+        home = mkExt4 "80%" "/home";
       };
     };
   };
